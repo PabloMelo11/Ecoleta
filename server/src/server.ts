41e9d1262724e7ad7e1e126496e40fc452b41db3
Pablo Melo <pablomelodev@gmail.com>
2020-06-30 19:46:28 -0300

    Finish server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,14 @@
-import express, { Request, Response } from "express";
+import path from "path";
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request: Request, response: Response) => {
-  return response.json({ message: "Hello World" });
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333, () => {
   console.log("Server is runnig in port 3333...");
