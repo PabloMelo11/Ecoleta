b29214447e96c3fce231ed2f0b75a178254fd5c5
PaabloMelo <pablo.melo@tangotech.com.br>
2021-01-02 14:45:26 -0300

    feat: add validation in routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, () => {
   console.log('Server is runnig in port 3333...');
